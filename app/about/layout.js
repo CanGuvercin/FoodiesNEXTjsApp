397871cbefb2b235b208e7a5dac3141e3d28e673
Can Guvercin <mustafacanguvercin@gmail.com>
2025-08-17 23:55:00 +0300

    more routiing

---
+++ app/about/layout.js
@@ -0,0 +1,15 @@
+export default function AbourtLayoutCapsule () {
+    return (
+        <div>
+        <h1>About NextLevel Food</h1>
+        <p>
+            NextLevel Food is a platform for foodies to share their favorite recipes with the world. 
+            It&apos;s a place to discover new dishes, and to connect with other food lovers.
+        </p>
+        <p>
+            Our mission is to create a community where food enthusiasts can come together, share their culinary experiences, 
+            and inspire each other to explore the diverse world of gastronomy.
+        </p>
+        </div>
+    );
+    }
